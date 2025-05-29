c077d90f8e6e8ad76e97e2e1aea25b2b789ee1c6
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-05-29 15:10:40 +0200

    Update filter export

--- _11ty/filters/related-posts.js
+++ _11ty/filters/related-posts.js
@@ -1,4 +1,4 @@
-export function relatedPostsFilter(collection = []) {
+export default function relatedPostsFilter(collection = []) {
 	const page = this.ctx.page;
 	return collection.filter(post => {
 		return post.url !== page.url;
