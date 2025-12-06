c0356da1354d9873bf0cbfd3f03c4275d57ce47c
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-06 14:35:30 +0100

    Toy around with virtual templates

--- _baseline/modules/navigator-core/plugins/navigator-core.js
+++ _baseline/modules/navigator-core/plugins/navigator-core.js
@@ -1,6 +1,26 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { getVerbose } from "../../../helpers.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
-export default function(eleventyConfig) {
+export default function(eleventyConfig, options = {}) {
   eleventyConfig.addNunjucksGlobal("_navigator", function() {
 		return this;
 	})
+
+	if (getVerbose(eleventyConfig)) {
+		// Read virtual template from file
+		const templatePath = path.join(__dirname, "../templates/core-navigator.html");
+		const virtualTemplateContent = fs.readFile(templatePath, "utf-8");
+	
+		eleventyConfig.addTemplate("core-navigator.html", virtualTemplateContent, {
+			permalink: "/core-navigator/",
+			title: "Core Navigator",
+			meta: "Very important page",
+		});
+	};
 }
