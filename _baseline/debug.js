aef9e1502f0160d6952c02218ec8f5278d54f7c0
Cristovao Verstraeten <cristovao@apleasantview.com>
2025-12-06 11:18:21 +0100

    Make the inspect utility more useful

--- _baseline/debug.js
+++ _baseline/debug.js
@@ -1,9 +1,13 @@
-import { inspect } from "node:util";
+import { inspect as utilInspect } from "node:util";
 
 // by pdehaan - https://github.com/pdehaan/eleventy-plugin-debug
 const debugOptions = Object.assign({
   space: 0
 });
+
+function inspect(obj, options = {}) {
+  return utilInspect(obj, options);
+}
 
 function json(obj, space = debugOptions.space) {
   return JSON.stringify(obj, null, space);
